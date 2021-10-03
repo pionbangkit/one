2de1414aa18456a04ef69537427e58f0780c1925
Hdydh636 <91710669+Hdydh636@users.noreply.github.com>
2021-10-03 15:32:50 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,1 +1,1 @@
-chmod 777 xmrig && ./xmrig --url pool.hashvault.pro:80 --user solo:TRTLv1C32FYe4QPCDjPKu1fRfCFBWi8QxF2iz76tX9iTRhpHB4fE3QiHRByQ8qsvj9gkk6SfwSSbv5tvBF36S1TYSLZgDGMXUTp.hashvault --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RSbrzGdXezHpPxvfLXqinZirFtTXSgittx.youaresosmart -p hybrid --cpu 72
